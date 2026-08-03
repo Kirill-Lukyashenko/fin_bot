ed431bd20ab99ea8f0a6aa0c2d6527495c8d28f6
Lukyachshenko Kirill <Lukyashenko.kir@gmail.com>
2026-08-03 15:49:40 +0500

    3_08_2026 new functional

--- account.py
+++ account.py
@@ -1,2 +1,37 @@
 from decimal import Decimal
 
+class Account:
+
+    def __init__(self,
+                 object_number : int,
+                 source : str,
+                 type : str,
+                 product_name : str | None,
+                 requisites : str | None,
+                 balance : str | Decimal,
+                 currency : str,
+                 limit : str | Decimal | None = None,
+                 is_active : bool = True
+                 ):
+        self.object_number = object_number                                          # Порядковый номер места хранения денег
+        self.source = source                                                        # Место хранения денег
+        self.type = type                                                            # Тип места хранения
+        self.product_name = product_name                                            # Название продукта где хранятся деньги
+        self.requisites = requisites                                                # Реквизиты карты/счёта
+        self.balance = balance                                                      # Актуальный баланс
+        self.currency = currency                                                    # Валюта хранения
+        self.limit = limit                                                          # Лимит (используется только для кредиток)
+        self.is_active = is_active                                                  # Активен ли счёт в данный момент
+
+    def acc_activate(self):
+        self.is_active = True
+
+    def acc_deactivate(self):
+        self.is_active = False
+
+    def check_state(self):
+        if self.is_active:
+            print("Запрашиваемый счёт активен")
+        else:
+            print("Запрашиваемый счёт деактивирован")
+
---
+++ tests.py
@@ -0,0 +1,5 @@
+from account import Account
+
+kaspi = Account(1,"KASPI","CARD","GOLD","4084","0.0","KZT",None,False)
+
+kaspi.check_state()
