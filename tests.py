eebb200bd201f86c1336a1b28553dc1e1287cc8c
Kirill Lukyachshenko <Lukyashenko.kir@gmail.com>
2026-08-04 00:13:10 +0500

    update for class transaction

--- tests.py
+++ tests.py
@@ -1,5 +1,9 @@
 from account import Account
+from transaction import Transaction, OperationType
+from datetime import date
 
 kaspi = Account(1,"KASPI","CARD","GOLD","4084","0.0","KZT",None,True)
+tr1 = Transaction(date(2026,8,3),"10.00",OperationType.INCOME,"Зарплата",kaspi,"Получил ЗП",None,False)
 
 print(kaspi.check_state())
+print(tr1.check_state())
--- transaction.py
+++ transaction.py
@@ -1,22 +1,57 @@
 from decimal import Decimal
 from account import Account
 from datetime import date
+from enum import Enum
+
+class OperationType(Enum):
+    """Перечисление типов доступных операций"""
+    INCOME = "Доход"
+    EXPENSE = "Расход"
 
 class Transaction:
 
     def __init__(self,
                  action_date : date,
                  amount : str | Decimal,
-                 operation : str,
+                 operation : OperationType,
                  category : str,
                  account : Account,
                  comment : str,
-                 transaction_id : int | None = None
+                 transaction_id : int | None = None,
+                 is_active : bool = True
                  ):
-        self.action_date = action_date
-        self.amount = Decimal(amount)
-        self.operation = operation
-        self.category = category
-        self.account = account
-        self.comment = comment
-        self.transaction_id = transaction_id
+        self.action_date = action_date                                  # Дата соверщения транзакции
+        self.amount = Decimal(amount)                                   # Сумма транзакции
+        self.operation = operation                                      # Тип операции
+        self.category = category                                        # Категория
+        self.account = account                                          # Счёт списания/пополнения
+        self.comment = comment.strip()                                  # Комментарий
+        self.transaction_id = transaction_id                            # Уникальный номер транзакции
+        self.is_active = is_active                                      # Состояние транзакции
+
+        # Проверка правильности переданной суммы
+        if self.amount <=0:
+            raise ValueError("Сумма не может быть меньше или равна нулю")
+
+        # Проверка правильности введеного коментария
+        if not self.comment:
+            raise ValueError("Комментарий не может быть пустым")
+
+        # Проверка правильности операции
+        if not isinstance(self.operation, OperationType):
+            raise ValueError("Неверная операция")
+
+    def check_state(self) -> str:
+        """Проверка состояния транзакции"""
+        if self.is_active :
+            return "Транзакция активна"
+        
+        return "Транзакция отменена"
+
+    def restore(self) -> None:
+        """Восстановление транзакции"""
+        self.is_active = True
+
+    def cancel(self) -> None:
+        """Отмена транзакции"""
+        self.is_active = False
