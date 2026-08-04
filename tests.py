8441f1432d2c3b65f2deb1d1e49a8337ef2c9323
Lukyachshenko Kirill <Lukyashenko.kir@gmail.com>
2026-08-04 13:57:49 +0500

    fin_bot_v_0_0_1

--- tests.py
+++ tests.py
@@ -2,8 +2,7 @@ from account import Account
 from transaction import Transaction, OperationType
 from datetime import date
 
-kaspi = Account(1,"KASPI","CARD","GOLD","4084","0.0","KZT",None,True)
-tr1 = Transaction(date(2026,8,3),"10.00",OperationType.INCOME,"Зарплата",kaspi,"Получил ЗП",None,False)
+kaspi = Account(1,"Kaspi","Карта","Kaspi gold", "4084","0.0","KZT", None)
 
-print(kaspi.check_state())
-print(tr1.check_state())
+income = Transaction(date(2026,8,4),"5000.00",OperationType.INCOME, "Зарплата", kaspi, "Получил зпшку")
+expense = Transaction(date(2026,8,4),"2000.00",OperationType.EXPENSE, "Продукты", kaspi, "Закупился в магазине")
--- transaction.py
+++ transaction.py
@@ -1,9 +1,9 @@
-from decimal import Decimal
+from decimal import Decimal, InvalidOperation
 from account import Account
 from datetime import date
 from enum import Enum
 
-class OperationType(Enum):
+class OperationType(str, Enum):
     """Перечисление типов доступных операций"""
     INCOME = "Доход"
     EXPENSE = "Расход"
@@ -11,35 +11,78 @@ class OperationType(Enum):
 class Transaction:
 
     def __init__(self,
-                 action_date : date,
-                 amount : str | Decimal,
-                 operation : OperationType,
-                 category : str,
-                 account : Account,
-                 comment : str,
-                 transaction_id : int | None = None,
-                 is_active : bool = True
-                 ):
-        self.action_date = action_date                                  # Дата соверщения транзакции
-        self.amount = Decimal(amount)                                   # Сумма транзакции
-        self.operation = operation                                      # Тип операции
-        self.category = category                                        # Категория
-        self.account = account                                          # Счёт списания/пополнения
-        self.comment = comment.strip()                                  # Комментарий
-        self.transaction_id = transaction_id                            # Уникальный номер транзакции
-        self.is_active = is_active                                      # Состояние транзакции
+                 action_date : date,                                            # Дата соверщения транзакции
+                 amount : str | Decimal,                                        # Сумма транзакции
+                 operation : OperationType,                                     # Операция
+                 category : str,                                                # Категория
+                 account : Account,                                             # Счёт списания/пополнения
+                 comment : str,                                                 # Комментарий
+                 transaction_id : int | None = None,                            # Уникальный номер транзакции
+                 is_active : bool = True                                        # Состояние транзакции
+                 ):                                                                                                                                                                                                                       
 
+        # Проверка правильности переданной даты
+        if not isinstance(action_date, date):
+            raise TypeError("Дата транзакции должна быть объектом date")
+
+        self.action_date = action_date 
+
+        # Проверка счёта
+        if not isinstance(account, Account):
+            raise TypeError("Счёт должен быть объектом класса Account")
+
+        self.account = account 
+
+        # Проверка категории
+        if not isinstance(category, str):
+            raise TypeError("Категория должна быть строкой")
+
+        self.category = category.strip()                                        
+
+        if not self.category:
+            raise ValueError("Категория не может быть пустой")
+        
         # Проверка правильности переданной суммы
+        try:
+            normalized_amount = str(amount).replace(" ", "").replace(",", ".")
+            self.amount = Decimal(normalized_amount)
+
+        except (InvalidOperation, TypeError, ValueError):
+            raise ValueError("Передано некорректное значение суммы")
+
         if self.amount <=0:
             raise ValueError("Сумма не может быть меньше или равна нулю")
 
         # Проверка правильности введеного коментария
+        if not isinstance(comment, str):
+            raise TypeError("Комментарий должен быть строкой")
+
+        self.comment = comment.strip()                                          
+
         if not self.comment:
             raise ValueError("Комментарий не может быть пустым")
 
         # Проверка правильности операции
-        if not isinstance(self.operation, OperationType):
+        if not isinstance(operation, OperationType):
             raise TypeError("Неверная операция")
+
+        self.operation = operation  
+
+        # Проверка правильности транзакции
+        if transaction_id is not None:
+            if not isinstance(transaction_id, int):
+                raise TypeError("Идентификатор транзакции должен быть int или None")
+
+            if transaction_id <= 0:
+                raise ValueError("Идентификатор транзакции должен быть больше нуля")
+
+        self.transaction_id = transaction_id
+
+        # Проверка правильности активного состояния
+        if not isinstance(is_active, bool):
+            raise TypeError("Состояние транзакции должно иметь тип bool")
+
+        self.is_active = is_active   
 
     def check_state(self) -> str:
         """Проверка состояния транзакции"""
