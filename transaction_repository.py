5bfccec7e96bf8533b837dea033a3b54ef484702
Lukyachshenko Kirill <Lukyashenko.kir@gmail.com>
2026-08-11 13:25:49 +0500

    fin_bot_v_0_0_8

--- transaction_repository.py
+++ transaction_repository.py
@@ -6,4 +6,54 @@ class TransactionRepository:
     """Описание работы с таблицей transactions"""
 
     def add_transactions(self, transaction : Transaction) -> int:
-        pass
+        """Функция добавляет в таблицу transactions новую транзакцию"""
+
+        if not isinstance(transaction, Transaction):
+            raise TypeError("Должен быть передан объект Transaction")
+
+        if transaction.transaction_id is not None:
+            raise ValueError("Данная транзакция уже имеет идетнификатор")
+
+        if transaction.account.object_number is None:
+            raise ValueError("Счёт транзакции сначала должен быть сохранен в базе")
+
+        amount_minor = to_minor_units(transaction.amount)
+
+        conection = get_connection()
+
+        try:
+
+            cursor = conection.execute(
+                """
+                INSERT INTO transactions (
+                    action_date,
+                    amount_minor,
+                    operation,
+                    category,
+                    acount_id,
+                    comment,
+                    is_active
+                )
+                VALUES (?,?,?,?,?,?,?)
+                """,
+                transaction.action_date.isoformat(),
+                amount_minor,
+                transaction.operation.value,
+                transaction.category,
+                transaction.account.object_number,
+                transaction.comment,
+                int(transaction.is_active),
+            )
+
+            conection.commit()
+
+            transaction.transaction_id = cursor.lastrowid
+
+            return transaction.transaction_id
+
+        except Exception:
+            conection.rollback()
+            raise
+
+        finally:
+            conection.close()
