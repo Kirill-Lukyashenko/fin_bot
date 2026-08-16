ab647c02e591f803b2c6d103426eba0de1e57502
Kirill Lukyachshenko <Lukyashenko.kir@gmail.com>
2026-08-16 22:51:59 +0500

    fin_bot_v_0_0_11

--- transaction_repository.py
+++ transaction_repository.py
@@ -1,4 +1,5 @@
 from transaction import Transaction, OperationType
+from account import Account
 from database import get_connection
 from money import to_minor_units, from_minor_units
 from datetime import date
@@ -204,3 +205,110 @@ class TransactionRepository:
                 transaction_id= transaction_row["id"],
                 is_active= bool(transaction_row["is_active"])
             )
+
+    def get_transactions_by_period(self, date_start: date, date_end: date, limit : int = 50, offset : int = 0) -> list[Transaction]:
+        """Функция возвращает список транзакций за временной период"""
+
+        if not isinstance(date_start, date):
+            raise TypeError("начальная дата должна быть объектом date")
+
+        if not isinstance(date_end, date):
+            raise TypeError("конечная дата должна быть объектом date")
+        
+        if date_start > date_end :
+            raise ValueError("Дата начала не может быть больше даты конечной")
+
+        if type(limit) is not int:
+            raise TypeError("Лимит должен быть целочисленной переменной")
+
+        if type(offset) is not int:
+            raise TypeError("Смещение должно быть целочисленной переменной")
+
+        if limit <= 0 :
+            raise ValueError("Лимит должен быть больше нуля")
+
+        if offset < 0 :
+            raise ValueError("Смещение не может быть отрицательным")
+
+        connection = get_connection()
+
+        try:
+
+            rows = connection.execute(
+                """
+                SELECT
+                    t.id AS transaction_id,
+                    t.action_date AS transaction_action_date,
+                    t.amount_minor AS transaction_amount_minor,
+                    t.operation AS transaction_operation,
+                    t.category AS transaction_category,
+                    t.comment AS transaction_comment,
+                    t.is_active AS transaction_is_active,
+
+                    a.id AS account_id,
+                    a.source AS account_source,
+                    a.acc_type AS account_type,
+                    a.product_name AS account_product_name,
+                    a.requisites AS account_requisites,
+                    a.currency AS account_currency,
+                    a.balance_minor AS account_balance_minor,
+                    a.limit_minor AS account_limit_minor,
+                    a.is_active AS account_is_active
+                
+                FROM transactions AS t
+
+                JOIN accounts AS a
+                    ON t.account_id = a.id
+
+                WHERE t.action_date >= ?
+                AND t.action_date <= ?
+
+                ORDER BY
+                    t.action_date DESC,
+                    t.id DESC
+
+                LIMIT ?
+                OFFSET ?
+                """,
+                (
+                    date_start.isoformat(),
+                    date_end.isoformat(),
+                    limit,
+                    offset,
+                 )
+            ).fetchall()
+            
+        finally:
+            connection.close()
+
+
+        transactions = []
+
+        for row in rows:
+
+            account = Account(
+                object_number=row["account_id"],
+                source= row["account_source"],
+                acc_type= row["account_type"],
+                product_name= row["account_product_name"],
+                requisites= row["account_requisites"],
+                balance= from_minor_units(row["account_balance_minor"]),
+                currency= row["account_currency"],
+                limit= (from_minor_units(row["account_limit_minor"]) if row["account_limit_minor"] is not None else None),
+                is_active= bool(row["account_is_active"])
+            )
+
+            transaction = Transaction(
+                action_date= date.fromisoformat(row["transaction_action_date"]),
+                amount= from_minor_units(row["transaction_amount_minor"]),
+                operation= OperationType(row["transaction_operation"]),
+                category= row["transaction_category"],
+                account= account,
+                comment= row["transaction_comment"],
+                transaction_id= row["transaction_id"],
+                is_active= bool(row["transaction_is_active"])
+            )
+
+            transactions.append(transaction)
+
+        return transactions
