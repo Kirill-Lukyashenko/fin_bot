ec71095b4f0ad2bd04a692b2d8368d8ae81835fd
Kirill Lukyachshenko <Lukyashenko.kir@gmail.com>
2026-09-20 23:15:48 +0500

    fin_bot_v_1_0_3

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent
 
 load_dotenv(BASE_DIR / ".env")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("DEV_BOT_HOME_TOKEN")
 
 if BOT_TOKEN is None:
     raise RuntimeError("Переменная BOT_TOKEN не найдена .env")
